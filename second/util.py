650f2b6ccca4ebf6e5be6947e69eb0c4e5877aba
Ben Weber <dev.ben.weber@gmail.com>
2016-11-14 17:33:00 +0100

    Added support for masks during loading
    This allows to load only particular parts of the brain (e.g. all
    non-zero dimensions) which significantly lowers the memory requirements

--- second/util.py
+++ second/util.py
@@ -20,21 +20,47 @@ TEST_COUNT = 138
 FEATURE_SPACE = X_DIM * Y_DIM * Z_DIM
 Z_PLANE_FEATURE_SPACE = X_DIM * Y_DIM
 
+__NNZ_MASK_MEMOIZATION = None
+
 
 def load_refs():
     return np.asarray([int(age) for age in open('%s/targets.csv' % DATA_PATH)])
 
 
-def load_nifti1(path):
-    return _load_nifti1(path).get_data().reshape(*DIMS)
+def load_nifti1(path, mask=None):
+    mri = _load_nifti1(path).get_data().reshape(*DIMS)
+
+    if mask is not None:
+        mri = mri[mask]
+
+    return mri
 
 
-def load_train(i):
-    return load_nifti1('%s/set_train/train_%d.nii' % (DATA_PATH, i + 1))
+def load_nnz_mask():
+    global __NNZ_MASK_MEMOIZATION
+
+    if __NNZ_MASK_MEMOIZATION is None:
+        __NNZ_MASK_MEMOIZATION = tuple(np.load('%s/nnz_mask.npy' % DATA_PATH))
+
+    return __NNZ_MASK_MEMOIZATION
 
 
-def load_test(i):
-    return load_nifti1('%s/set_test/test_%d.nii' % (DATA_PATH, i + 1))
+def get_mask_count(mask):
+    return mask[0].shape[0]
+
+
+def load_train(i, mask=None):
+    return load_nifti1(
+        '%s/set_train/train_%d.nii' % (DATA_PATH, i + 1),
+        mask=mask
+    )
+
+
+def load_test(i, mask=None):
+    return load_nifti1(
+        '%s/set_test/test_%d.nii' % (DATA_PATH, i + 1),
+        mask=mask
+    )
 
 
 def save(data, filename):
@@ -65,11 +91,43 @@ def load_all_train(observations_axis=1, dtype=np.float64):
         )
 
 
+def load_all_nnz_train(observations_axis=1, dtype=np.float64):
+    nnz_mask = load_nnz_mask()
+    nnz_count = get_mask_count(nnz_mask)
+
+    def load(i):
+        return load_train(i, mask=nnz_mask)
+
+    return (
+        load_dataset(
+            (nnz_count, TRAIN_COUNT), load,
+            observations_axis=observations_axis, dtype=dtype
+        ),
+        nnz_mask
+    )
+
+
 def load_all_test(observations_axis=1, dtype=np.float64):
     return load_dataset(
-            (FEATURE_SPACE, TEST_COUNT), load_test,
+        (FEATURE_SPACE, TEST_COUNT), load_test,
+        observations_axis=observations_axis, dtype=dtype
+    )
+
+
+def load_all_nnz_test(observations_axis=1, dtype=np.float64):
+    nnz_mask = load_nnz_mask()
+    nnz_count = get_mask_count(nnz_mask)
+
+    def load(i):
+        return load_test(i, mask=nnz_mask)
+
+    return (
+        load_dataset(
+            (nnz_count, TEST_COUNT), load,
             observations_axis=observations_axis, dtype=dtype
-        )
+        ),
+        nnz_mask
+    )
 
 
 def load_full_dataset(observations_axis=1, dtype=np.float64):
@@ -81,9 +139,28 @@ def load_full_dataset(observations_axis=1, dtype=np.float64):
             return load_test(i - TRAIN_COUNT)
 
     return load_dataset(
-            (FEATURE_SPACE, TRAIN_COUNT + TEST_COUNT), load,
+        (FEATURE_SPACE, TRAIN_COUNT + TEST_COUNT), load,
+        observations_axis=observations_axis, dtype=dtype
+    )
+
+
+def load_full_nnz_dataset(observations_axis=1, dtype=np.float64):
+    nnz_mask = load_nnz_mask()
+    nnz_count = get_mask_count(nnz_mask)
+
+    def load(i):
+        if i < TRAIN_COUNT:
+            return load_train(i, mask=nnz_mask)
+        else:
+            return load_test(i - TRAIN_COUNT, mask=nnz_mask)
+
+    return (
+        load_dataset(
+            (nnz_count, TRAIN_COUNT + TEST_COUNT), load,
             observations_axis=observations_axis, dtype=dtype
-        )
+        ),
+        nnz_mask
+    )
 
 
 def load_z_plane(i, z=int(Z_DIM/2), train_data=True):
@@ -113,6 +190,7 @@ def load_all_z_planes(
                 (Z_PLANE_FEATURE_SPACE, TEST_COUNT), _load_z_plane,
                 observations_axis=observations_axis, dtype=dtype
             )
+
 
 def nonzero_rv(data, observations_axis=1):
     nnz = np.nonzero(np.sum(data, axis=observations_axis))[0]
