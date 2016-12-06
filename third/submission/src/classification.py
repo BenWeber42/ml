8d9a6dc7eba7aeb07360051d1e54c653f81df31d
Ben Weber <dev.ben.weber@gmail.com>
2016-12-06 19:06:21 +0100

    Added final classification of mlp2 & code cleanup

--- third/submission/src/classification.py
+++ third/submission/src/classification.py
@@ -1,7 +1,67 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -W ignore::DeprecationWarning
+import numpy as np
+import util
+import classifier_pool as classifiers
+from sklearn.pipeline import Pipeline
+from pipeline import (
+    DataPipeline,
+    FeatureExtractors
+)
+np.random.seed(1)
+# Data hyper-parameters
+TRAIN_COUNT = 278
+TEST_COUNT = 138
+
+PLANES_Z = list(range(20, 170))  # Load planes.
+
+ESTIMATOR_POOL = {
+    'svm': False,
+    'random_forest': True,
+    'adaboost': False,
+    'knn': False,
+}
+
+
 def main():
-    # FIXME: entry point here, create the final submission file here
-    print('Hello, World')
+    ####################################
+    # Prepare feature matrices.
+    ####################################
+    print('Loading Data.')
+    training_targets = util.load_refs()  # Load targets
+
+    data_pipeline = DataPipeline()
+    feature_extractors = FeatureExtractors(data_pipeline, {
+        FeatureExtractors.HISTOGRAMS: {
+            'partitions': [(9, 9, 9)],
+            'bins': [30],
+            'interval': [(1, 2000)]
+        },
+    })
+    pipeline = Pipeline([
+        ('data_dict_builder', data_pipeline.data_dict_builder),
+        ('feature_extraction', feature_extractors.get_pipeline()),
+    ])
+    print('Creating Feature Matrix.')
+    features_all = pipeline.transform(data_pipeline.all_data)
+
+    training_feature_matrix = features_all[:TRAIN_COUNT, :]
+    test_feature_matrix = features_all[TRAIN_COUNT:, :]
+
+    print('Training feature matrix dimensionality '
+          + str(training_feature_matrix.shape))
+    print('Classification.')
+    if ESTIMATOR_POOL['svm'] is True:
+        classifiers.svm(
+            training_feature_matrix, training_targets, test_feature_matrix)
+    if ESTIMATOR_POOL['random_forest'] is True:
+        classifiers.random_forest(
+            training_feature_matrix, training_targets, test_feature_matrix)
+    if ESTIMATOR_POOL['knn'] is True:
+        classifiers.knn(
+            training_feature_matrix, training_targets, test_feature_matrix)
+    if ESTIMATOR_POOL['adaboost'] is True:
+        classifiers.adaboost(
+            training_feature_matrix, training_targets, test_feature_matrix)
 
 
 if __name__ == '__main__':
--- third/submission/src/edgeDetector.py
+++ third/submission/src/edgeDetector.py
@@ -2,73 +2,75 @@ import numpy as np
 import util
 import histograms
 import cv2
-from matplotlib import pyplot as plt
-
-# image type init16
-dtype=np.float64
 
 
-def detectEdges(volume, settings=(1,1,1)):
-    
-    dims = util.DIMS
+def detectEdges(volume, settings=(1, 1, 1)):
+
     maxValue = np.max(volume)
-    
-    mri = np.zeros(util.DIMS, dtype=dtype)
-    
-    parameterA = 150 #lower
-    parameterB = 200 #higher
-    
-    
+
+    mri = np.zeros(util.DIMS, dtype=np.float64)
+
+    lowThreshold = 150
+    highThreshold = 200
+
     if settings[0] == 1:
-        for x in range(0, dims[0]-1):
-            img = volume[x,:,:]      
-            cvuint8 = cv2.convertScaleAbs(img, 255.0/maxValue )
-            edges = cv2.Canny(cvuint8,parameterA,parameterB)  
-            mri[x,:,:] = mri[x,:,:] + edges
-    
+        for x in range(util.X_DIM):
+            img = volume[x, :, :]
+            cvuint8 = cv2.convertScaleAbs(img, 255.0/maxValue)
+            edges = cv2.Canny(cvuint8, lowThreshold, highThreshold)
+            mri[x, :, :] += edges
+
     if settings[1] == 1:
-        for y in range(0, dims[1]-1):
-            img = volume[:,y,:]      
-            cvuint8 = cv2.convertScaleAbs(img, 255.0/maxValue )
-            edges = cv2.Canny(cvuint8,parameterA,parameterB)  
-            mri[:,y,:] = mri[:,y,:] + edges
-    
+        for y in range(util.Y_DIM):
+            img = volume[:, y, :]
+            cvuint8 = cv2.convertScaleAbs(img, 255.0/maxValue)
+            edges = cv2.Canny(cvuint8, lowThreshold, highThreshold)
+            mri[:, y, :] += edges
+
     if settings[2] == 1:
-        for z in  range(0, dims[2]-1):
-            img = volume[:,:,z]      
-            cvuint8 = cv2.convertScaleAbs(img, 255.0/maxValue )
-            edges = cv2.Canny(cvuint8,parameterA,parameterB)  
-            mri[:,:,z] = mri[:,:,z] + edges
-    
-    
-    mri2 = mri > (256) #edge should be detected on two different slices containing the point (x,y,z)  (e.g. in plane xy and plane yz that contains the point (x,y,z))
-    mri2 = mri2.astype(int)    
+        for z in range(util.Z_DIM):
+            img = volume[:, :, z]
+            cvuint8 = cv2.convertScaleAbs(img, 255.0/maxValue)
+            edges = cv2.Canny(cvuint8, lowThreshold, highThreshold)
+            mri[:, :, z] += edges
+
+    # edge should be detected on two different slices containing the point
+    # (x, y, z)  (e.g. in plane xy and plane yz that contains the point
+    # (x, y, z))
+    mri2 = mri > (256)
+    mri2 = mri2.astype(int)
 
     return mri2
 
 
-def generateEdgeFeaturesVector(volume, detectorSettings=(1,1,1), partitions=(9,9,9) ):
+def generateEdgeFeaturesVector(
+    volume,
+    detectorSettings=(1, 1, 1),
+    partitions=(9, 9, 9)
+):
     mri = detectEdges(volume, detectorSettings)
     mri_partitioner = histograms.UniformPartitioner(partitions)
-    partitions = mri_partitioner.get_parts(volume)
-    
+    partitions = mri_partitioner.get_parts(mri)
+
     features = []
     for partition in partitions:
         features.append(np.count_nonzero(partition)/partition.size)
     return features
 
 
-def generateFullTrainMatrix(detectorSettings=(1,1,1), partitions=(9,9,9)):
+def generateFullTrainMatrix(detectorSettings=(1, 1, 1), partitions=(9, 9, 9)):
     trainMatrix = []
     for i in range(0, util.TRAIN_COUNT-1):
         trainMatrix.append(generateEdgeFeaturesVector(util.load_train(i)))
     return trainMatrix
-    
-def generateFullTestMatrix(detectorSettings=(1,1,1), partitions=(9,9,9)):
+
+
+def generateFullTestMatrix(detectorSettings=(1, 1, 1), partitions=(9, 9, 9)):
     testMatrix = []
     for i in range(0, util.TEST_COUNT-1):
         testMatrix.append(generateEdgeFeaturesVector(util.load_test(i)))
     return testMatrix
+
 
 def generateAndSaveFull():
     fullTrainMatrix = generateFullTrainMatrix()
@@ -78,4 +80,3 @@ def generateAndSaveFull():
     fullTestMatrix = generateFullTestMatrix()
     np.save("data\\test_full_edge", fullTestMatrix)
     print("test_full_edge saved", np.shape(fullTestMatrix))
-
