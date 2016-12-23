a965c78420ada176847109e4f7f4bf7df92e6e5d
Ben Weber <dev.ben.weber@gmail.com>
2016-12-23 16:46:40 +0100

    Updated checkSub & submission script

--- third/make_submission.sh
+++ third/make_submission.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 rm -rf ./MLP2_LittleBigData.zip
 cd submission
-git archive -o ../MLP2_LittleBigData.zip HEAD .
+git archive -o ../MLP3_LittleBigData.zip HEAD .
 cd ../
 git submodule init
 git submodule update
-python ./checkSub/checkSub.py MLP2_LittleBigData.zip
+python ./checkSub/checkSub.py MLP3_LittleBigData.zip
 
 # TODO: adjust readme
 # TODO: adjust final_sub.csv
 # TODO: adjust src/classification.py
-# TODO: adjust checkSub for MLP3
-# TODO: this script for MLP3
